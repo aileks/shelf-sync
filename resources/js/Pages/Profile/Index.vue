31a56be5ea47efcb82f224f7623199368f477979
aileks <88669313+aileks@users.noreply.github.com>
2024-01-31 11:01:49 -0500

    started profile page

---
+++ resources/js/Pages/Profile/Index.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <main>
+    <aside>
+      SideNav title goes here
+      <ul>
+        <li>Settings</li>
+        <li>Log Out</li>
+      </ul>
+    </aside>
+  </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,10 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Home
 Route::get('/', fn () => Inertia::render('Home'))->name('Home');
+
+// Books stuff
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/add', [BookController::class, 'add']);
 Route::post('/books/add', [BookController::class, 'store']);
@@ -14,6 +17,10 @@ Route::get('/books/edit/{book}', [BookController::class, 'edit']);
 Route::patch('/books/edit', [BookController::class, 'update']);
 Route::delete('/books/{book}', [BookController::class, 'destroy']);
 
+// Profile stuff
+Route::get('/profile', fn () => Inertia::render('Profile/Index'));
+
+// Auth stuff
 Route::get('/register', fn () => Inertia::render('Auth/Register'));
 Route::post('/register', function () {
     $attributes = request()->validate([
