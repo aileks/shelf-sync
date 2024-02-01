b0290209967a4cdb746edad7add2b3001d4462c6
aileks <88669313+aileks@users.noreply.github.com>
2024-02-01 15:40:48 -0500

    back to horizontal nav bar for now

--- resources/js/Pages/Shared/NavBar.vue
+++ resources/js/Pages/Shared/NavBar.vue
@@ -1,55 +1,39 @@
-<script setup></script>
+<script setup>
+import StyledButton from "@/Shared/StyledButton.vue";
+</script>
 
 <template>
   <nav
-    class="fixed top-0 left-0 h-full space-y-8 border-r-2 w-[120px] border-bronze bg-primary"
+    class="flex px-1 py-2 mx-2 mt-2 space-x-4 rounded-md bg-sandy shadow-paper"
   >
-    <div class="flex flex-col items-center justify-between h-full">
-      <div class="flex flex-col flex-1 h-full">
-        <span class="text-center">LOGO</span>
-        <ul class="flex-1 px-1 mx-1 mt-4 font-medium text-center">
-          <li v-show="!$page.props.auth.user" class="my-3">
-            <Link
-              class="font-bold transition-all duration-200 hover:bg-brown hover:text-neutral-50 hover:rounded-md rounded-sm hover:py-1 hover:px-2.5 hover:shadow-paper text-brown"
-              href="/register"
-              >Register</Link
-            >
-          </li>
+    <div class="mr-auto">
+      <StyledButton v-show="!$page.props.auth.user">
+        <Link href="/register"> Register </Link>
+      </StyledButton>
 
-          <li v-show="!$page.props.auth.user" class="my-3">
-            <Link
-              class="font-bold transition-all duration-200 hover:bg-brown hover:text-neutral-50 hover:rounded-md rounded-sm hover:py-1 hover:px-2.5 hover:shadow-paper text-brown"
-              href="/login"
-              >Log In</Link
-            >
-          </li>
+      <StyledButton v-show="!$page.props.auth.user">
+        <Link href="/login"> Log In </Link>
+      </StyledButton>
 
-          <li v-show="$page.props.auth.user" class="my-3">
-            <Link
-              class="font-bold transition-all duration-200 hover:bg-brown hover:text-neutral-50 hover:rounded-md rounded-sm hover:py-1 hover:px-2.5 hover:shadow-paper text-brown"
-              href="/books"
-              >Book List</Link
-            >
-          </li>
-
-          <li v-show="$page.props.auth.user" class="my-3">
-            <Link
-              method="POST"
-              class="font-bold transition-all duration-200 hover:bg-brown hover:text-neutral-50 hover:rounded-md rounded-sm hover:py-1 hover:px-2.5 hover:shadow-paper text-brown"
-              href="/logout"
-              >Log Out</Link
-            >
-          </li>
-        </ul>
-      </div>
-      <div>
-        <Link href="/profile">
-          <img
-            src="https://randomuser.me/api/portraits/women/79.jpg"
-            class="w-12 h-12 rounded-full"
-          />
+      <StyledButton v-show="$page.props.auth.user">
+        <Link as="button" href="/logout" method="post" type="button">
+          Log Out
         </Link>
-      </div>
+      </StyledButton>
+    </div>
+
+    <div class="ml-auto">
+      <StyledButton v-show="$page.props.auth.user">
+        <Link href="/books/add"> Add Book </Link>
+      </StyledButton>
+
+      <StyledButton v-show="$page.props.auth.user">
+        <Link href="/books"> View Books </Link>
+      </StyledButton>
+
+      <StyledButton v-show="$page.props.auth.user">
+        <Link href="/profile"> Profile </Link>
+      </StyledButton>
     </div>
   </nav>
 </template>
