79b7e3c69df015dcc254c27092793e8893ae6d0f
aileks <88669313+aileks@users.noreply.github.com>
2024-02-06 12:26:02 -0500

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,7 @@
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 
 <body class="font-sans antialiased">
