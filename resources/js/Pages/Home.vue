aeabcb2074c962591264dfd858e514be17dd20c4
Aaliyah Harper <aeverly14@pm.me>
2024-08-08 14:54:33 -0400

    no more footer outside of homepage

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -92,6 +92,35 @@
         </div>
       </main>
     </div>
+
+    <footer
+      class="absolute left-0 right-0 flex flex-col bg-tan text-sm text-neutral-800 dark:bg-dark-bronze dark:text-neutral-50"
+    >
+      <span
+        class="my-2 flex items-center justify-center text-center sm:flex-wrap"
+      >
+        Please report any issues on&nbsp;&nbsp;
+        <a
+          @mouseover="isHovered = true"
+          @mouseleave="isHovered = false"
+          class="mt-[3px] flex items-center justify-center align-bottom text-blue hover:underline dark:text-dark-blue"
+          href="https://github.com/aileks/quillify/"
+        >
+          GitHub.
+          <ArrowUpRightIcon
+            class="h-4 w-4"
+            :class="{
+              '-translate-y-0.5 translate-x-0.5 transform transition-transform duration-150':
+                isHovered,
+            }"
+          />
+        </a>
+      </span>
+
+      <p class="mb-2 text-center">
+        &copy; 2024 Aaliyah Harper. All rights reserved.
+      </p>
+    </footer>
   </Layout>
 </template>
 
--- resources/js/Pages/Layouts/Layout.vue
+++ resources/js/Pages/Layouts/Layout.vue
@@ -12,33 +12,4 @@
   <div class="relative mt-4 text-center">
     <slot />
   </div>
-
-  <footer
-    class="absolute left-0 right-0 flex flex-col bg-bronze text-sm text-neutral-50 dark:bg-dark-bronze"
-  >
-    <span
-      class="my-2 flex items-center justify-center text-center sm:flex-wrap"
-    >
-      Please report any issues on&nbsp;&nbsp;
-      <a
-        @mouseover="isHovered = true"
-        @mouseleave="isHovered = false"
-        class="mt-[3px] flex items-center justify-center align-bottom text-blue hover:underline dark:text-dark-blue"
-        href="https://github.com/aileks/quillify/"
-      >
-        GitHub.
-        <ArrowUpRightIcon
-          class="h-4 w-4"
-          :class="{
-            '-translate-y-0.5 translate-x-0.5 transform transition-transform duration-150':
-              isHovered,
-          }"
-        />
-      </a>
-    </span>
-
-    <p class="mb-2 text-center">
-      &copy; 2024 Aaliyah Harper. All rights reserved.
-    </p>
-  </footer>
 </template>
