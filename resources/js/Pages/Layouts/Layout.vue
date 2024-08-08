5bb98f325880ca89d270b34dfb063e872deb7c68
Aaliyah Harper <aeverly14@pm.me>
2024-08-08 09:13:11 -0400

    primary color switch up for dark mode

--- resources/js/Pages/Layouts/Layout.vue
+++ resources/js/Pages/Layouts/Layout.vue
@@ -1,5 +1,6 @@
 <script setup>
   import NavBar from '../Components/NavBar.vue';
+  import { ArrowUpRightIcon } from '@heroicons/vue/20/solid';
 </script>
 
 <template>
@@ -8,4 +9,31 @@
   <div class="mt-4 p-2 text-center">
     <slot />
   </div>
+
+  <footer
+    class="fixed bottom-0 left-0 right-0 mb-1 flex flex-1 flex-col items-center justify-center text-sm dark:text-neutral-50"
+  >
+    <span
+      class="my-2 flex items-center justify-center text-center sm:flex-wrap"
+    >
+      Please report any issues on&nbsp;
+      <a
+        @mouseover="isHovered = true"
+        @mouseleave="isHovered = false"
+        class="ml-[1px] mt-[1px] flex items-center text-blue hover:underline dark:text-dark-green"
+        href="https://github.com/aileks/quillify/"
+      >
+        GitHub.
+        <ArrowUpRightIcon
+          class="h-4 w-4"
+          :class="{
+            '-translate-y-0.5 translate-x-0.5 transform transition-transform duration-150':
+              isHovered,
+          }"
+        />
+      </a>
+    </span>
+
+    <p class="text-center">&copy; 2024 Aaliyah Harper. All rights reserved.</p>
+  </footer>
 </template>
