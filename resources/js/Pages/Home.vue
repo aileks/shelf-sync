fe8f975c0c32c70288a915295dc7f047da898ae8
Aaliyah Harper <aeverly14@pm.me>
2024-08-07 21:53:44 -0400

    fixed spacing on mobile home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -15,18 +15,20 @@
   <Head title="Your Personal Library" />
 
   <Layout>
-    <div class="relative z-0 h-[60vh] w-auto sm:h-[40vh] md:h-[50vh]">
+    <div
+      class="relative z-0 flex w-auto flex-1 flex-col items-center justify-center"
+    >
       <img
         src="https://raw.githubusercontent.com/aileks/quillify/main/public/library.jpeg"
-        class="gradient absolute inset-0 h-1/2 w-full rounded-md object-cover sm:h-full"
+        class="absolute inset-0 w-full rounded-md object-cover sm:h-full"
       />
 
-      <div
-        class="absolute inset-0 h-1/2 rounded-md bg-gradient-to-r from-gray-900/80 to-gray-800/80 sm:h-full"
-      ></div>
+      <!-- <div -->
+      <!--   class="absolute inset-0 rounded-md bg-gradient-to-r from-gray-900/80 to-gray-800/80" -->
+      <!-- ></div> -->
 
       <header
-        class="flex h-1/2 w-full flex-col items-center justify-center text-neutral-50 sm:h-full"
+        class="text-shadow text-overlay z-10 my-2 ml-4 mr-3 flex flex-col items-center justify-center place-self-center py-4 text-neutral-50 sm:py-0"
       >
         <Transition
           enter-active-class="transition-opacity duration-1000 ease-in-out"
@@ -34,12 +36,12 @@
           leave-active-class="transition-opacity duration-700 ease-in-out"
           leave-to-class="opacity-0"
         >
-          <div v-show="animate" class="z-10">
-            <h1 class="my-4 text-2xl font-bold sm:text-5xl">
+          <div v-show="animate" class="z-10 h-full w-full px-4 text-center">
+            <h1 class="my-2 text-xl font-bold sm:my-4 sm:text-2xl md:text-5xl">
               Organize Your Reading Life with Quillify
             </h1>
 
-            <p class="my-12 text-lg sm:text-2xl">
+            <p class="my-8 pt-4 text-base sm:my-12 sm:text-lg md:text-2xl">
               Track and share your literary journey
             </p>
 
@@ -56,31 +58,33 @@
     </div>
 
     <main
-      class="relative flex flex-col items-center justify-center overflow-hidden"
+      class="relative mt-4 flex flex-col items-center justify-center overflow-hidden sm:mt-10"
     >
       <img
-        class="absolute left-0 top-5 h-full w-full -rotate-2 rounded-lg object-cover opacity-80 drop-shadow-md"
+        class="absolute left-0 top-0 h-full w-full -rotate-2 rounded-lg object-cover opacity-80 drop-shadow-md"
         src="https://raw.githubusercontent.com/aileks/quillify/main/public/table.png"
         alt="table of books"
       />
 
       <div
-        class="relative my-10 w-3/4 rounded-lg bg-sandy px-8 py-12 sm:h-full"
+        class="relative my-4 w-11/12 rounded-lg bg-sandy px-4 py-6 sm:my-10 sm:w-3/4 sm:px-8 sm:py-12"
       >
-        <p class="px-4 text-base font-medium sm:text-lg">
+        <p class="px-2 font-medium sm:px-4 sm:text-base sm:text-lg">
           Quillify is your simple, elegant solution for tracking your reading
           journey. Effortlessly manage your book collection our searchable list
           and keep your reading goals on track.
         </p>
 
-        <p class="mt-4 px-4 text-base font-medium sm:text-lg">
+        <p
+          class="mt-2 px-2 font-medium sm:mt-4 sm:px-4 sm:text-base sm:text-lg"
+        >
           Discover a more organized and enjoyable reading experience with
           Quillify.
         </p>
 
         <Link href="/register">
           <button
-            class="relative mt-8 rounded-md bg-green px-4 py-2 font-bold text-white shadow-paper transition-all duration-300 hover:bg-green/90 hover:shadow-none"
+            class="relative mt-4 rounded-md bg-green px-4 py-2 font-bold text-white shadow-paper transition-all duration-300 hover:bg-green/90 hover:shadow-none sm:mt-8"
           >
             Start Now!
           </button>
@@ -88,7 +92,9 @@
       </div>
     </main>
 
-    <footer class="mt-12 flex flex-col items-center justify-center text-sm">
+    <footer
+      class="absolute bottom-0 left-0 right-0 mb-2 flex flex-col items-center justify-center text-sm sm:mt-12"
+    >
       <span
         class="my-2 flex items-center justify-center text-center sm:flex-wrap"
       >
@@ -116,3 +122,15 @@
     </footer>
   </Layout>
 </template>
+
+<style scoped>
+  .text-overlay {
+    background: rgba(0, 0, 0, 0.7);
+    padding: 1rem;
+    border-radius: 0.25rem;
+  }
+
+  .text-shadow {
+    text-shadow: 2px 2px 4px rgb(0, 0, 0);
+  }
+</style>
